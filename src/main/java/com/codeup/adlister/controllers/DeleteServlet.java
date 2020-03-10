240631272dd1c6ef7264db6309d6fb2a6cf01297
Miguel Vera <miguel.vera7908@gmail.com>
2020-03-10 16:46:29 -0500

    update

--- src/main/java/com/codeup/adlister/controllers/DeleteServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteServlet.java
@@ -6,8 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class DeleteServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        if
-    }
-}
+//public class DeleteServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+//        if
+//    }
+//}
